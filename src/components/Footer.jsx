d9cae716452abc52fe3ed8ae787fff789eaa232e
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-10-18 22:23:27 -0700

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 - Faique Inaas MZM. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
